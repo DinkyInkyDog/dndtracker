550d533d164a6f393d4754ff269630dc225ae580
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-08-18 10:06:13 -0400

    button attempt

---
+++ src/main/java/com/dinkyinkydog/entity/Character.java
@@ -0,0 +1,14 @@
+package com.dinkyinkydog.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Character {
+	private int maxHealth;
+	private int savedCurrentHealth;
+	
+	
+	
+}
--- src/main/java/com/dinkyinkydog/views/track/TrackView.java
+++ src/main/java/com/dinkyinkydog/views/track/TrackView.java
@@ -1,5 +1,9 @@
 package com.dinkyinkydog.views.track;
 
+import com.dinkyinkydog.entity.Health;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -11,17 +15,23 @@ import com.vaadin.flow.router.RouteAlias;
 @RouteAlias(value = "")
 public class TrackView extends HorizontalLayout {
 
-    
+    private Health health = new Health(16);
     
     
 
     public TrackView() {
     	
-    	
-    	
-    	 int intValue = 42; // Your int value
-         NativeLabel label = new NativeLabel("The value is: " + intValue);
-         add(label);
+         NativeLabel maxHealthLabel = new NativeLabel("" + health.getMaxHealth());
+         NativeLabel currentHealthLabel = new NativeLabel("" + health.getCurrentHealth());
+         add(maxHealthLabel);
+         add(currentHealthLabel);
+         String brokenHeart = "img/Icon.brokenheart.png";
+         Image takeDamageIcon = new Image(brokenHeart, "broken heart");
+         takeDamageIcon.setWidth("100px");
+         Button takeDamageButton = new Button(takeDamageIcon);
+         takeDamageButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+         
+         add(takeDamageButton);
     }
 
 }
