810666568187a83642fbc6553ec4ca853afe29e9
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-08-21 21:49:26 -0400

    create css and split components
    I started getting everything put into containers. It looks like its good but now Tomcat is failing to start. Going to close eclipse and open it again to see if that fixes it.

--- src/main/java/com/dinkyinkydog/views/track/TrackView.java
+++ src/main/java/com/dinkyinkydog/views/track/TrackView.java
@@ -2,17 +2,22 @@ package com.dinkyinkydog.views.track;
 
 import com.dinkyinkydog.entity.Health;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 @PageTitle("Track")
 @Route(value = "")
 @RouteAlias(value = "")
+@Theme(value = "lumo", variant = Lumo.DARK)
+@CssImport("./styles/myStyles.css")
 public class TrackView extends HorizontalLayout {
 
     private Health health = new Health(16);
@@ -21,11 +26,20 @@ public class TrackView extends HorizontalLayout {
 
     public TrackView() {
     	
+    	//--containers---
+    	Div mainPage = new Div();
+    	mainPage.setClassName("main-page");
+    	
+    	Div healthcontrols = new Div();
+    	Div currentHealthDisplay = new Div();
+    	currentHealthDisplay.setClassName("health-display");
+    	
+    	//---max health state----
          NativeLabel maxHealthLabel = new NativeLabel("Max: " + health.getMaxHealth());
          NativeLabel currentHealthLabel = new NativeLabel("" + health.getCurrentHealth());
+         currentHealthLabel.setClassName("centered-label");
          Image hp = new Image("icons/HPicon.png", "currentHealthIcon");
-         add(maxHealthLabel);
-         add(currentHealthLabel);
+         
          
          
          
@@ -35,14 +49,21 @@ public class TrackView extends HorizontalLayout {
          Image takeDamageIcon = new Image("img/Icon.brokenheart.png", "broken heart");
          takeDamageIcon.setWidth(buttonSize);
          Button takeDamageButton = new Button(takeDamageIcon);
-         takeDamageButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+         
          
          Image healthpotionIcon = new Image("img/icon.potion.png", "potion bottle");
          healthpotionIcon.setWidth(buttonSize);
          Button healButton = new Button(healthpotionIcon);
          
-         add(takeDamageButton);
-         add(healButton);
+         
+         
+         //--view---
+         mainPage.add(currentHealthDisplay, healthcontrols);
+         healthcontrols.add(maxHealthLabel, healButton, takeDamageButton);
+         currentHealthDisplay.add(currentHealthLabel);
+         
+         add(mainPage);
+         
     }
 
 }
