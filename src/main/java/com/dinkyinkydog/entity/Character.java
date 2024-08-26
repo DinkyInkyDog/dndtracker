76caae18f546356f9134d9bd7c6b99093e03679f
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-08-26 00:20:50 -0400

    modify magic
    I came up with a better way to keep track of the magic. Instead of having it be a bunch of ints

--- src/main/java/com/dinkyinkydog/entity/Character.java
+++ src/main/java/com/dinkyinkydog/entity/Character.java
@@ -6,9 +6,47 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Character {
+	public enum Classes {
+		DRUID, CLERIC, BARD, BARBARIAN, FIGHTER, PALADIN, ROGUE, MONK, RANGER, SORCERER, WARLOCK, WIZARD, UNDEFINED
+	}
+	
 	private int maxHealth;
 	private int savedCurrentHealth;
+	private String _class;
 	
+	public Classes get_class() {
+		switch(_class) {
+		case "druid":
+			return Classes.DRUID;
+		case "cleric":
+			return Classes.CLERIC;
+		case "barbarian":
+			return Classes.BARBARIAN;
+		case "bard":
+			return Classes.BARD;
+		case "fighter":
+			return Classes.FIGHTER;
+		case "paladin":
+			return Classes.PALADIN;
+		case "rogue":
+			return Classes.ROGUE;
+		case "monk":
+			return Classes.MONK;
+		case "ranger":
+			return Classes.RANGER;
+		case "sorcerer":
+			return Classes.SORCERER;
+		case "warlock":
+			return Classes.WARLOCK;
+		case "wizard":
+			return Classes.WIZARD;
+		default:
+			return Classes.UNDEFINED;
+		}
+	}
+
 	
-	
+	public Character(String _class) {
+		set_class(_class);
+	}
 }
--- src/main/java/com/dinkyinkydog/entity/Magic.java
+++ src/main/java/com/dinkyinkydog/entity/Magic.java
@@ -1,12 +1,22 @@
 package com.dinkyinkydog.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.dinkyinkydog.entity.Character.Classes;
+
 public class Magic {
-	private int level1;
-	private int level2;
-	private int level3;
-	private int level4;
-	private int level5;
-	private int level6;
-	private int level7;
-	private int level8;
+	Set<SpellSlot> level1 = new HashSet<>();
+	
+	
+	
+	public Magic(int level, Classes _class) {
+		
+	}
+	
+	
+	public class SpellSlot {
+		private int level;
+		private boolean active;
+	}
 }
