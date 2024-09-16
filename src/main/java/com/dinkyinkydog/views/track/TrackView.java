b8bf239100fc4d6ca90a64996d7ef7c9f326ea87
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-09-16 06:05:44 -0400

    nullpointerexception handled
    I got rid of the nullpointerexception by adding an if statement and also made it so that the number field goes back to null after the button is pressed.

--- src/main/java/com/dinkyinkydog/views/track/TrackView.java
+++ src/main/java/com/dinkyinkydog/views/track/TrackView.java
@@ -19,66 +19,70 @@ import com.vaadin.flow.router.RouteAlias;
 @CssImport("./styles/myStyles.css")
 public class TrackView extends HorizontalLayout {
 
-    private Health health = new Health(16);
-    
-    
+	private Health health = new Health(16);
 
-    public TrackView() {
-    	
-    	//--containers---
-    	Div mainPage = new Div();
-    	mainPage.setClassName("main-page");
-    	
-    	Div healthcontrols = new Div();
-    	Div currentHealthDisplay = new Div();
-    	currentHealthDisplay.setClassName("health-display");
-    	
-    	//---max health state----
-         NativeLabel maxHealthLabel = new NativeLabel("Max: " + health.getMaxHealth());
-         maxHealthLabel.addClassName("max_health_label");
-         NativeLabel currentHealthLabel = new NativeLabel("" + health.getCurrentHealth());
-         currentHealthLabel.setClassName("centered-label");
-         Image hp = new Image("icons/HPicon.png", "currentHealthIcon");
-         hp.setClassName("health-image");
-         
-         
-         
-         //---change number input---
-         NumberField healthChange = new NumberField();
-         healthChange.setWidth("50px");
-         
-         
-         //heal/damage buttons
-         String buttonSize = "70px";
-         
-         Image takeDamageIcon = new Image("img/Icon.brokenheart.png", "broken heart");
-         takeDamageIcon.setWidth(buttonSize);
-         Button takeDamageButton = new Button(takeDamageIcon);
-         takeDamageButton.addClickListener(event ->{ 
-        	 double value = healthChange.getValue();
-        	 health.takeDamage((int)value);
-        	 currentHealthLabel.setText("" + health.getCurrentHealth());
-        	 Notification.show("Damage subtracted from health");
-        	 });
-         
-         Image healthpotionIcon = new Image("img/icon.potion.png", "potion bottle");
-         healthpotionIcon.setWidth(buttonSize);
-         Button healButton = new Button(healthpotionIcon);
-         healButton.addClickListener(event -> {
-        	 double value = healthChange.getValue(); 
-        	 health.heal((int)value);
-        	 currentHealthLabel.setText("" + health.getCurrentHealth());
-        	 Notification.show("Health added to health");
-         });
-         
-         
-         //--view---
-         mainPage.add(currentHealthDisplay, healthcontrols);
-         healthcontrols.add(healButton,healthChange, takeDamageButton);
-         currentHealthDisplay.add(hp,currentHealthLabel);
-         
-         add(mainPage);
-         
-    }
+	public TrackView() {
+
+		// --containers---
+		Div mainPage = new Div();
+		mainPage.setClassName("main-page");
+
+		Div healthcontrols = new Div();
+		Div currentHealthDisplay = new Div();
+		currentHealthDisplay.setClassName("health-display");
+
+		// ---max health state----
+		NativeLabel maxHealthLabel = new NativeLabel("Max: " + health.getMaxHealth());
+		maxHealthLabel.addClassName("max_health_label");
+		NativeLabel currentHealthLabel = new NativeLabel("" + health.getCurrentHealth());
+		currentHealthLabel.setClassName("centered-label");
+		Image hp = new Image("icons/HPicon.png", "currentHealthIcon");
+		hp.setClassName("health-image");
+
+		// ---change number input---
+		NumberField healthChange = new NumberField();
+		healthChange.setWidth("50px");
+
+		// heal/damage buttons
+		String buttonSize = "70px";
+
+		Image takeDamageIcon = new Image("img/Icon.brokenheart.png", "broken heart");
+		takeDamageIcon.setWidth(buttonSize);
+		Button takeDamageButton = new Button(takeDamageIcon);
+		takeDamageButton.addClickListener(event -> {
+			if (healthChange.getValue() != null) {
+				double value = healthChange.getValue();
+				health.takeDamage((int) value);
+				currentHealthLabel.setText("" + health.getCurrentHealth());
+				Notification.show("Damage subtracted from health");
+				healthChange.setValue(null);
+			} else {
+				Notification.show("Please enter a number in the feild");
+			}
+		});
+
+		Image healthpotionIcon = new Image("img/icon.potion.png", "potion bottle");
+		healthpotionIcon.setWidth(buttonSize);
+		Button healButton = new Button(healthpotionIcon);
+		healButton.addClickListener(event -> {
+			if (healthChange.getValue() != null) {
+				double value = healthChange.getValue();
+				health.heal((int) value);
+				currentHealthLabel.setText("" + health.getCurrentHealth());
+				Notification.show("Health added to health");
+				healthChange.setValue(null);
+			} else {
+				Notification.show("Please enter a number in the feild");
+			}
+		});
+
+		// --view---
+		mainPage.add(currentHealthDisplay, healthcontrols);
+		healthcontrols.add(healButton, healthChange, takeDamageButton);
+		currentHealthDisplay.add(hp, currentHealthLabel);
+
+		add(mainPage);
+
+	}
 
 }
